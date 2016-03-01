24da07a6292e5e733da9e259791d88d3efa9d52d
genych <evgeny.she+git@gmail.com>
2016-03-01 17:55:05 +0200

    Add new test

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import TradingCards as tc
+import gui
+import os
+
 
 def test_packs():
     for pack in tc.packs.values():
@@ -6,6 +9,10 @@ def test_packs():
         for card in cards:
             print('{} from {}'.format(card.cardName, pack.packName))
 
+def test_download():
+    image = gui.get_image('dummy')
+    assert os.path.exists(image)
 
 if __name__ == '__main__':
-    test_packs()
+    # test_packs()
+    test_download()
